ac535ee696bb10982835101024016c30d91fc9b9
Aniket Maurya <theaniketmaurya@gmail.com>
2023-02-09 19:38:48 +0530

    fix

--- research_app/components/model_demo.py
+++ research_app/components/model_demo.py
@@ -32,7 +32,7 @@ class ModelDemo(ServeGradio):
 
     inputs = gr.inputs.Image(type="pil", label="Upload Image for Object Detection")
     outputs = gr.outputs.Image(type="pil", label="Output Image")
-    examples = [["zidane.jpg"], [["bus.jpg"]]]
+    # examples = [["zidane.jpg"], [["bus.jpg"]]]
     enable_queue = True
 
     def __init__(self):
@@ -46,8 +46,7 @@ class ModelDemo(ServeGradio):
 
     def yolo(self, image):
         results = self.model(image, size=640)  # includes NMS
-        results.render()
-        return Image.fromarray(results.imgs[0])
+        return Image.fromarray(results.render()[0])
 
     def predict(self, image) -> str:
         return self.yolo(image)
